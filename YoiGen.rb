e6bb846c4e6e394bc37e04a782f9cbaf27b93b33
Jurre <scpcb173@gmail.com>
2019-02-02 23:28:00 +0100

    Working on main LFO

--- YoiGen.rb
+++ YoiGen.rb
@@ -9,7 +9,6 @@
 
 volume = 2
 beatsperminute = 140
-waitTime = 1
 
 #Synth settings
 
@@ -19,22 +18,25 @@ wtPitch = 28
 wtRelease = 0.7
 wtAmplitude = 2
 
-sampleRate = 2000
-synthDepth = -2
+fxSampleRate = 2000
+fxFlangDepth = -2
+fxFlangDecay = 2
+fxFlangFeedback = 0.7
+
 
 
 set_volume! volume
 set :bpm , beatsperminute
 use_bpm get(:bpm)
 
-with_fx :gverb, spread: 0.5, room: 10, mix: 0.5, damp: 0.5, release: 0.3 do
-  with_fx :flanger, decay: 2, feedback: 0.7, mix: 1, phase: 2, wave: 3, depth: synthDepth do
-    with_fx :bitcrusher, mix: 1, sample_rate: sampleRate, bits: 16 do
+with_fx :gverb, mix: 0.5, spread: 0.5, room: 10, damp: 0.5, release: 0.3 do
+  with_fx :flanger, mix: 1, decay: fxFlangDecay, feedback: fxFlangFeedback, phase: 2, wave: 3, depth: fxFlangDepth do
+    with_fx :bitcrusher, mix: 1, sample_rate: fxSampleRate, bits: 16 do
       with_fx :wobble, phase_offset: 0.5, phase: 1, filter: 0, mix: 1, cutoff_min: 10, cutoff_max: 80, res: 0.75, wave: 3 do
         live_loop :synths do
           use_synth :square
-          play wtPitch, amp: wtAmplitude, release: wtRelease
-          wait waitTime
+          play wtPitch, amp: wtAmplitude, release: (wtRelease / lfo)
+          wait (1.0 / lfo)
         end
       end
     end
