9f191151e7e7aac1b74dd81226d27e24e013c47f
Jurre <scpcb173@gmail.com>
2019-02-02 22:52:29 +0100

    Changed to reverb fx

--- RiddimGen.rb
+++ RiddimGen.rb
@@ -28,7 +28,7 @@ hpfEnabled = 1
 
 live_loop :synths do
   with_fx :wobble, mix: wobbleEnabled, phase_offset: length, phase: (length * 2), filter: 0, cutoff_min: 10, cutoff_max: 80, res: 0.75, wave: 3 do
-    with_fx :gverb, mix: (reverbEnabled / 1.25), damp: 0.75, room: 5, release: 2 do
+    with_fx :reverb, mix: (reverbEnabled / 1.7), damp: 0.5, room: 0.6, release: 2 do
       with_fx :flanger, mix: flangerEnabled, decay: 2, feedback: 0.7, phase: 0.5, wave: 3, depth: -1.6 do
         with_fx :hpf, mix: hpfEnabled, cutoff: 50 do
           with_fx :eq, mix: eqEnabled, high_note: 110, low_shelf: 1.5, high_shelf: 3 do
