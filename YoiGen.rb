5c76a94d3f91b87e2c26e81015c89f00fecee51d
Jurre <scpcb173@gmail.com>
2019-02-03 00:11:50 +0100

    Changing value scale with multiplications

--- YoiGen.rb
+++ YoiGen.rb
@@ -14,12 +14,13 @@ beatsperminute = 140
 
 #Synth settings
 
-lfo = 1 # Value ranging from 0 to 2 (if integer)
+lfo = 4.0 # Value ranging above 0 (MUST BE A FLOAT)
 
 wtPitch = 28 # Recommended from 25 to 35
 wtRelease = 0.7
 
-fxSampleRate = 2000 # Recommended from 1500 to 4000
+fxSampleRate = 2 # Recommended from 1.5 to 3.5
+fxCutoffMultiplier = 0.24 # Value ranging from 0 to 1
 fxFlangDepth = -2
 fxFlangDecay = 2
 fxFlangFeedback = 0.7
@@ -30,15 +31,17 @@ fxFlangWavetype = 3 # Value from 1, 2 or 3
 set_volume! volume
 set :bpm , beatsperminute
 use_bpm get(:bpm)
-
+fxCutoffMultiplication = (1 + fxCutoffMultiplier)
+fxSampleRateMultiplication = (fxSampleRate * 1000)
+lfoMultiplication = (lfo / 4)
 with_fx :gverb, mix: 0.5, spread: 0.5, room: 10, damp: 0.5, release: 0.3 do
-  with_fx :flanger, mix: 1, decay: fxFlangDecay, feedback: fxFlangFeedback, phase: (2 / lfo), wavetype: fxFlangWavetype, depth: fxFlangDepth do
-    with_fx :bitcrusher, mix: 1, sample_rate: fxSampleRate, bits: 16 do
-      with_fx :wobble, phase_offset: 0.5, phase: (1.0 / lfo), filter: 0, mix: 1, cutoff_min: 10, cutoff_max: 80, res: 0.75, wave: 3 do
+  with_fx :flanger, mix: 1, decay: fxFlangDecay, feedback: fxFlangFeedback, phase: (2 / lfoMultiplication), wavetype: fxFlangWavetype, depth: fxFlangDepth do
+    with_fx :bitcrusher, mix: 1, sample_rate: fxSampleRateMultiplication, bits: 16 do
+      with_fx :wobble, phase_offset: 0.5, phase: (1.0 / lfoMultiplication), filter: 0, mix: 1, cutoff_min: (5 * fxCutoffMultiplication), cutoff_max: (64.5 * fxCutoffMultiplication), res: 0.75, wave: 3 do
         live_loop :synths do
           use_synth :square
-          play wtPitch, amp: 2, release: (wtRelease / lfo)
-          wait (1.0 / lfo)
+          play wtPitch, amp: 2, release: (wtRelease / lfoMultiplication)
+          wait (1.0 / lfoMultiplication)
         end
       end
     end
