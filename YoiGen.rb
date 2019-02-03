1b60c2b5dc19a876b35cfff149a01e498c5eb7c9
Jurre <scpcb173@gmail.com>
2019-02-03 11:13:47 +0100

    Made pitch variable at better scale

--- YoiGen.rb
+++ YoiGen.rb
@@ -17,7 +17,7 @@ extraWaitTime = 0
 
 lfo = 4.0 # Value ranging above 0 (MUST BE A FLOAT)
 
-wtPitch = 28 # Recommended from 25 to 35
+wtPitch = 8 # Recommended from 0 to 10
 wtRelease = 0.7
 
 fxSampleRate = 2 # Recommended from 1.5 to 3.5
@@ -35,13 +35,14 @@ use_bpm get(:bpm)
 fxCutoffMultiplication = (1 + fxCutoffMultiplier)
 fxSampleRateMultiplication = (fxSampleRate * 1000)
 lfoMultiplication = (lfo / 4)
+wtPitchMultiplication = (20 + wtPitch)
 with_fx :gverb, mix: 0.5, spread: 0.5, room: 10, damp: 0.5, release: 0.3 do
   with_fx :flanger, mix: 1, decay: fxFlangDecay, feedback: fxFlangFeedback, phase: (2 / lfoMultiplication), wavetype: fxFlangWavetype, depth: fxFlangDepth do
     with_fx :bitcrusher, mix: 1, sample_rate: fxSampleRateMultiplication, bits: 16 do
       with_fx :wobble, phase_offset: 0.5, phase: (1.0 / lfoMultiplication), filter: 0, mix: 1, cutoff_min: (5 * fxCutoffMultiplication), cutoff_max: (64.5 * fxCutoffMultiplication), res: 0.75, wave: 3 do
         live_loop :synths do
           use_synth :square
-          play wtPitch, amp: 2, release: (wtRelease / lfoMultiplication)
+          play wtPitchMultiplication, amp: 2, release: (wtRelease / lfoMultiplication)
           wait ((1.0 / lfoMultiplication) + extraWaitTime)
         end
       end
